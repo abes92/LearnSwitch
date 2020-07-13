d3cb5925c879a9728066aceb00ad2b2bece3389e
abesTaieb92 <abestaieb92@gmail.com>
2020-07-13 12:17:10 -0400

    switch exercice

---
+++ src/Switch/LearnSwitch.java
@@ -0,0 +1,26 @@
+package Switch;
+
+public class LearnSwitch {
+  public static void main(String args []){
+      int motivation=4;
+      switch(motivation){
+          case 1:
+              System.out.println("naver give up");
+              break;
+          case 2:
+              System.out.println("you have not because you ask not");
+              break;
+          case 3:
+              System.out.println("be patien for the decision of your lord");
+          break;
+          case 4:
+              System.out.println("live and die on this day");
+              break;
+          default:
+              System.out.println("calm down");
+              break;
+      }
+      }
+
+
+}
